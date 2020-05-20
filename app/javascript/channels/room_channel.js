51ad5b76a4af4f8e2cedb22db6c0268efbc1703f
kudojp <heyjudejudejude1968@gmail.com>
2020-05-20 23:30:37 +0900

    Disable submit button when input is empty

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -25,9 +25,28 @@ document.addEventListener('turbolinks:load', () => {
   const documentElement = document.documentElement
   // サーバーからのレスポンスのjs内でも使用できるように変数を決定
   window.messageContent = document.getElementById('message_content')
+
   // ページの一番下までスクロールする関数。サーバーからのレスポンスのjs内でも使用できるように変数を決定
   window.scrollToBottom = () => {
     window.scroll(0, documentElement.scrollHeight)
   }
   scrollToBottom()
+
+  const messageButton = document.getElementById('message-button')
+  // 空欄でなければボタンを有効化、空欄なら無効化する関数
+  const button_activation = () => {
+    if (messageContent.value === '') {
+      messageButton.classList.add('disabled')
+    } else {
+      messageButton.classList.remove('disabled')
+    }
+  }
+  // フォームに入力した際の動作
+  messageContent.addEventListener('input', () => {
+    button_activation()
+  })
+  // 送信ボタンが押された後にボタンを無効化
+  messageButton.addEventListener('click', () => {
+    messageButton.classList.add('disabled')
+  })
 })
